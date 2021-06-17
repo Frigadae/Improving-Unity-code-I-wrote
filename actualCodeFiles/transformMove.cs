7b2c97469c206681646aa9ea8fe1cab7eec45b9e
Daniel Wang <42727264+wang-yi-yao@users.noreply.github.com>
2021-06-18 09:58:05 +1200

    Uploaded wrong file

--- actualCodeFiles/transformMove.cs
+++ actualCodeFiles/transformMove.cs
@@ -2,22 +2,58 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FollowPlayer : MonoBehaviour
+public class NewMoveScript : MonoBehaviour
 {
-    public Transform player;
-    public float zCoord = -1;
+    public float runSpeed;
+    public float jumpSpeed;
+    public Transform feet;
+    public LayerMask Ground;
+
+    private Rigidbody2D playableObject;
+    private SpriteRenderer playableSprite;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        playableSprite = GetComponent<SpriteRenderer>();
+        playableObject = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        transform.position = new Vector3(player.position.x, player.position.y, zCoord);
+        float xMove = Input.GetAxis("Horizontal");
 
-        //new Vector3(player.position.x, player.position.y, z);
+        //Jump
+        if (Input.GetKey("up") && IsGrounded())
+        {
+            playableObject.velocity = new Vector2(xMove * runSpeed, jumpSpeed);
+        }
+
+        //Horizontal Movement
+        playableObject.velocity = new Vector2(xMove * runSpeed, playableObject.velocity.y);
+
+        //Flip sprite
+        if (xMove > 0)
+        {
+            playableSprite.flipX = false;
+        }
+        else if (xMove < 0)
+        {
+            playableSprite.flipX = true;
+        }
+    }
+    public bool IsGrounded()
+    {
+        Collider2D groundCheck = Physics2D.OverlapCircle(feet.position, 0.5f, Ground);
+
+        if (groundCheck == null)
+        {
+            return false;
+        } 
+        else
+        {
+            return true;
+        }
     }
 }
